2943b1ebf3a504710a28942bba8bcea270d442b0
Timothy Headley <theadley@jhb.dvt.co.za>
2023-03-02 09:33:52 +0200

    feat: ✨ Added code for observables and rxjs lesson

---
+++ observables/src/models/flight.d.ts
@@ -0,0 +1,25 @@
+export interface IFlightAPIResponse {
+  time: number;
+  states: (string | number | boolean | number[])[][];
+}
+
+export interface IFlight {
+  icao24: string;
+  callsign: string;
+  origin_country: string;
+  time_position: number;
+  last_contact: number;
+  longitude: number;
+  latitude: number;
+  baro_altitude: number;
+  on_ground: boolean;
+  velocity: number;
+  true_track: number;
+  vertical_rate: number;
+  sensors: number[];
+  geo_altitude: number;
+  squawk: string;
+  spi: boolean;
+  position_source: number;
+  category: number;
+}
---
+++ observables/src/utils/utils.ts
@@ -0,0 +1,5 @@
+export class Utils {
+  static convertToKM(meters: number): number {
+    return meters / 1000;
+  }
+}
